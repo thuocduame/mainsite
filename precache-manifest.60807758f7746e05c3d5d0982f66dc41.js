08fc3b62eba422e4fab993adc4d6d157d6d7763c
poynt2005 <admin@poynt2005.cf>
2020-06-11 12:55:09 +0800

    Updates

---
+++ precache-manifest.60807758f7746e05c3d5d0982f66dc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28387c10fe9f2912b55f9712c844f77",
+    "url": "/mainsite/index.html"
+  },
+  {
+    "revision": "657f206e9ae409246ca3",
+    "url": "/mainsite/static/css/main.12742727.chunk.css"
+  },
+  {
+    "revision": "9795f5229238d48ec0e6",
+    "url": "/mainsite/static/js/2.442b9061.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mainsite/static/js/2.442b9061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657f206e9ae409246ca3",
+    "url": "/mainsite/static/js/main.27d145f1.chunk.js"
+  },
+  {
+    "revision": "e3dcc64dd4896509d433",
+    "url": "/mainsite/static/js/runtime-main.6c622f1d.js"
+  }
+]);
